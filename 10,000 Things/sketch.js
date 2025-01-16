fe046c168aa992d57d0a5a747df8e6329e1a7bae
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-01-16 11:54:55 -0500

    initial concept
    draws 10k circles each frame

--- 10,000 Things/sketch.js
+++ 10,000 Things/sketch.js
@@ -3,5 +3,22 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0);
+  strokeWeight(2);
+  for(let i = 0; i < 10000; i++) {
+    drawShape(createShape());
+  }
+}
+
+function createShape(){
+  let x = random(400);
+  let y = random(400); 
+  let d = random(10, 20);
+  let c = { r: random(255), g: random(255), b: random(255)}
+  return { x, y, d, c };
+}
+
+function drawShape(shape){
+  fill(shape.c.r, shape.c.g, shape.c.b);
+  circle(shape.x, shape.y, shape.d);
 }
