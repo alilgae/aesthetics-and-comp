8582cbc267e2f4a7c4c0b3c1fb3040dea660ab1b
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-01-21 14:03:59 -0500

    convert from rgb to hsb dot color generation

--- 10,000 Things/sketch.js
+++ 10,000 Things/sketch.js
@@ -7,6 +7,7 @@ const maxShapes = 10000;
 
 function setup() {
   createCanvas(400, 400);
+  colorMode(HSB)
   shapes = 0;
   lowerDiameterBound = 20;
   upperDiameterBound = 40;
@@ -19,6 +20,7 @@ function draw() {
     upperDiameterBound = 40;
     shapes = 0;
     clear();
+    colorMode(HSB)
   }
   if(shapes < maxShapes) drawShape(createShape())
   else animateClear()
@@ -28,17 +30,18 @@ function createShape(){
   let x = random(400);
   let y = random(400); 
   let d = random(10, 20);
-  let c = { r: random(255), g: random(255), b: random(255)}
-  return { x, y, d, c };
+  let h = random(360)
+  return { x, y, d, h };
 }
 
 function drawShape(shape){
-  fill(shape.c.r, shape.c.g, shape.c.b);
+  fill(shape.h, 45, 100)
   circle(shape.x, shape.y, shape.d);
   shapes++;
 }
 
 function animateClear() {
+  colorMode(RGB)
   fill(0,0,0,100);
   circle(random(400), random(400), random(lowerDiameterBound, upperDiameterBound))
   lowerDiameterBound+=5;
