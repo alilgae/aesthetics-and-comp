557242e8b7de20bf1cb90571b6744ce60f8eed49
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-01-25 23:08:14 -0500

    added perlin noise

--- Randomness + Mystical/sketch.js
+++ Randomness + Mystical/sketch.js
@@ -1,7 +1,25 @@
+let xScale = 0.015;
+let yScale = 0.02;
+
+let gap = 4;
+let offset = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  noStroke();
+  
+  fill(0);
+
+  //perlin noise circles 
+  for (let x = gap / 2; x < width; x += gap) {
+    for (let y = gap / 2; y < height; y += gap) {
+      let noiseValue = noise((x + offset) * xScale, (y + offset) * yScale, millis() / 2500);
+      let diameter = noiseValue * gap;
+      circle(x, y, diameter);
+    }
+  }
 }
