8f20be34a7c748959817ca965f0cc8dbeeb1a49b
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-01-25 23:26:36 -0500

    random color circles in perlin noise

--- Randomness + Mystical/sketch.js
+++ Randomness + Mystical/sketch.js
@@ -1,25 +1,53 @@
 let xScale = 0.015;
 let yScale = 0.02;
 
-let gap = 4;
+let gap = 10;
 let offset = 0;
 
+const colors = [200, 0, 110, 325, 270, 165, 25];
+let timer;
+let hue;
+let desireHue;
+let startingHue;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB)
+  timer = 0;
+  startingHue = colors[floor(random(colors.length))];
+  desireHue = getDesireHue()
+  hue = startingHue
 }
 
 function draw() {
-  background(255);
+  background(0);
   noStroke();
-  
-  fill(0);
+  timer += deltaTime / 5000
+  if(timer <= 1) {
+    hue = lerp(startingHue, desireHue, timer)
+  }
+  else {
+    startingHue = hue;
+    desireHue = getDesireHue()
+    timer-=1;
+  }
+  fill(hue, 100, 75);
 
   //perlin noise circles 
   for (let x = gap / 2; x < width; x += gap) {
     for (let y = gap / 2; y < height; y += gap) {
-      let noiseValue = noise((x + offset) * xScale, (y + offset) * yScale, millis() / 2500);
+      let noiseValue = noise((x + offset) * xScale, (y + offset) * yScale, millis() / 3000);
       let diameter = noiseValue * gap;
       circle(x, y, diameter);
     }
   }
+
+}
+
+function getDesireHue() {
+  let color = startingHue;
+  while(color == startingHue){
+    color = colors[floor(random(colors.length))]
+  }
+  return color;
 }
