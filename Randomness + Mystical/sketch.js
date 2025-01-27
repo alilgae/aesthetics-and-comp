0e2e6d0bb187e3db79bc797a993ff2718fec4477
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-01-27 10:52:52 -0500

    added whispy accent line

--- Randomness + Mystical/sketch.js
+++ Randomness + Mystical/sketch.js
@@ -10,6 +10,14 @@ let hue;
 let desireHue;
 let startingHue;
 
+let lineD
+const lineWidths = [15, 25, 50, 75]
+let lines = []
+let currentLine = []
+let lineCount = 0;
+const lineColors = [15, 75, 90, 120, 140, 160, 175, 190, 215, 240, 250, 270, 285, 300, 315, 345]
+let lineColor
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB)
@@ -17,10 +25,14 @@ function setup() {
   startingHue = colors[floor(random(colors.length))];
   desireHue = getDesireHue()
   hue = startingHue
+  lineD = lineWidths[floor(random(lineWidths.length))]
+  let firstCircle = addLine()
+  accentLine(firstCircle.x, firstCircle.y)
+  lineColor = lineColors[floor(random(lineColors.length))]
 }
 
 function draw() {
-  background(0);
+  background(230, 100, 20);
   noStroke();
   timer += deltaTime / 5000
   if(timer <= 1) {
@@ -31,7 +43,7 @@ function draw() {
     desireHue = getDesireHue()
     timer-=1;
   }
-  fill(hue, 100, 75);
+  fill(hue, 25, 75);
 
   //perlin noise circles 
   for (let x = gap / 2; x < width; x += gap) {
@@ -41,6 +53,7 @@ function draw() {
       circle(x, y, diameter);
     }
   }
+  accentLine(currentLine[currentLine.length-1].x, currentLine[currentLine.length-1].y)
 
 }
 
@@ -50,4 +63,36 @@ function getDesireHue() {
     color = colors[floor(random(colors.length))]
   }
   return color;
+}
+
+function accentLine(x, y) {
+  stroke(lineColor, 25, 100, 2 / lineD)
+  strokeWeight(lineD)
+  if(y > 0) {
+    x += (floor(random(0, 2)) - 0.5) * 25
+    y += (floor(random(0, 2))) * -10
+  } else {
+    lines.push(currentLine)
+    lineCount++
+    currentLine = []
+    let circle = addLine()
+    x = circle.x
+    y = circle.y
+  }
+  
+  currentLine.push({x, y})
+
+  for(let j = 0; j < lines.length; j++) {
+    for(let i = 1; i < lines[j].length; i++) {
+      line(lines[j][i].x, lines[j][i].y, lines[j][i-1].x, lines[j][i-1].y)
+    }
+  }
+  
+  for(let i = 1; i < currentLine.length; i++) {
+    line(currentLine[i].x, currentLine[i].y, currentLine[i-1].x, currentLine[i-1].y)
+  }
+}
+
+function addLine() {
+  return {x: random(100, width-100), y: random(height-100, height)}
 }
