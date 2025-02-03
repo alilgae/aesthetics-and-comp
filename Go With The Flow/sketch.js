12526159c98fe294a7bc27fb2e6827c6392baeb3
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-02-02 21:49:28 -0500

    create rings of flow

--- Go With The Flow/sketch.js
+++ Go With The Flow/sketch.js
@@ -1,8 +1,10 @@
 const NOISE_SCALE = 0.005
-const LINE_LENGTH = 50
-const LINE_GAP = 7.5
+const LINE_LENGTH = 25
+const LINE_GAP = 7.5 // range 1 -> 7.5
 const WEIGHT = 75
-const center = {x: 400, y: 300}
+
+const CENTER = {x: 400, y: 300}
+const RADIUS = 20;
 
 let field = []
 
@@ -11,12 +13,14 @@ function setup() {
   angleMode(DEGREES)
   strokeWeight(5)
 
-  for(let i = 0; i < 360; i+=LINE_GAP) {
-    let xPos = (center.x + (cos(i) * LINE_LENGTH))
-    let yPos = (center.y + (sin(i) * LINE_LENGTH))
-    let noiseDisplacement = map(noise(xPos*NOISE_SCALE, yPos*NOISE_SCALE), 0, 1, -WEIGHT, WEIGHT)
-    
-    field.push({x: xPos + noiseDisplacement, y: yPos + noiseDisplacement })
+  for(let r = RADIUS; r < 600; r+=RADIUS){
+    for(let i = 0; i < 360; i+=(LINE_GAP * 360)/(TAU * r)) {
+      let xPos = (CENTER.x + (cos(i) * (LINE_LENGTH + r)))
+      let yPos = (CENTER.y + (sin(i) * (LINE_LENGTH + r)))
+      let noiseDisplacement = map(noise(xPos*NOISE_SCALE, yPos*NOISE_SCALE), 0, 1, -WEIGHT, WEIGHT)
+      
+      field.push({x: xPos + noiseDisplacement, y: yPos + noiseDisplacement })
+    }
   }
 }
 
@@ -24,7 +28,7 @@ function draw() {
   background(255);
 
   //plot center for visualizing
-  point(center.x, center.y)
+  // point(CENTER.x, CENTER.y)
 
   //draw circular field
   field.forEach(l => point(l.x, l.y))
