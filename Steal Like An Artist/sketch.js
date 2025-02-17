77a2d56c5ae2b1e7264eeac434511d723c677ac9
KaidenT <89858394+alilgae@users.noreply.github.com>
2025-02-17 18:17:04 -0500

    changed ping-pong animation to infinite loop

--- Steal Like An Artist/sketch.js
+++ Steal Like An Artist/sketch.js
@@ -6,9 +6,10 @@ const N_INDEX = 0;
 const HUE_RANGE = [{min: 65, max: 125}, {min: 155, max: 250}, {min: 25, max: 50}, 
   {min: 285, max: 360}, {min: 135, max: 200}, {min: 235, max: 275}]
 
-let timer = 0
+let z = 0
 let zScale = 1;
 let hueRange;
+let zRange;
 
 function setup() {
   createCanvas(400, 400);
@@ -23,35 +24,49 @@ function setup() {
   colorMode(HSB)
 
   hueRange = HUE_RANGE[floor(random(0, HUE_RANGE.length))]
+  zRange = width / 2
 }
 
 function draw() {
-  timer+=zScale
+  z+=zScale
 
   for(let x = 0; x < width; x+=PIXEL_SCALE) {
     for(let y = 0; y < height; y+=PIXEL_SCALE){
       let distances = []
       for(let i = 0; i < NUM_POINTS; i++) {
         let seedPoint = SEED_POINTS[i]
-        let z = (timer)
         distances.push(dist(x, y, z, seedPoint.x, seedPoint.y, seedPoint.z))
       }
       let sorted = sort(distances)
-      let hue = map(sorted[N_INDEX], 0, width/3, hueRange.min, hueRange.max)
-      let saturation  = map(sorted[N_INDEX + 1], 0, width/2, 25, 50)
-      let brightness = map(sorted[N_INDEX + 2], 0, width/2, 25, 100)
+      let hue = map(sorted[N_INDEX], 0, zRange, hueRange.min, hueRange.max)
+      let saturation  = map(sorted[N_INDEX + 1], 0, zRange, 25, 50)
+      let brightness = map(sorted[N_INDEX + 2], 0, zRange, 25, 100)
 
       fill(hue, saturation, brightness)
       rect(x, y, PIXEL_SCALE, PIXEL_SCALE)
     }
   }
 
-  if(timer > width) zScale = -1
-  if(timer < 0) zScale = 1
+  updatePoints()
+
+  // if(z > width) zScale = -1
+  // if(z < 0) zScale = 1
 
   // show the points: 
   // fill('pink')
   // SEED_POINTS.forEach(p => circle(p.x, p.y, 10))
+}
+
+function updatePoints() {
+  for(let i = 0; i < NUM_POINTS; i++) {
+    if(SEED_POINTS[i].z < (z - zRange)) {
+      let point = SEED_POINTS[i]
+      SEED_POINTS.splice(i, 1)
+      point.z += random(width, width * 1.5)
+      SEED_POINTS.push(point)
+      console.log(point)
+    }
+  }
 }
 
 // Inspo: 
